4e4b3d1c55f8b6430718177cd94128c791d20320
mck0324 <dch3030@naver.com>
2023-06-04 21:41:02 +0900

    API Swaager

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Toy example')
+    .setDescription('채팅 개발을 위한 API 문서')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`listening on port ${port}`);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,32 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('api/users')
 export class UsersController {
     constructor(private userService: UsersService) {
     }
 
+    @ApiOperation({ summary: '내 정보 조회' })
     @Get()
     getUsers(@Req() req) {
         return req.user;
     }
 
+    @ApiOperation({ summary: '회원가입' })
     @Post()
     postUsers(@Body() data: JoinRequestDto) {
         this.userService.postUsers(data.email, data.nickname, data.password);
     }
 
+    @ApiOperation({ summary: '로그인' })
     @Post('login')
-    logIn() {
+    logIn(@Req() req) {
         return req.user;
     }
 
+    @ApiOperation({ summary: '로그아웃' })
     @Post('logout')
     logOut(@Req() req, @Res() res) {
         req.logOut();
