c3a7ff455d6a8f05a5a6ebd020f276875ee77bc1
mck0324 <dch3030@naver.com>
2023-06-04 22:47:50 +0900

    interceptor 기능

---
+++ src/common/interceptors/undefinedToNull.interceptors.ts
@@ -0,0 +1,12 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable, map } from "rxjs";
+
+@Injectable()
+export class UndefinedToNullInterceptor implements NestInterceptor{
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        return next.handle().pipe(map((data) => data === undefined ? null : data));
+    }
+}
+
+//data == user면
+//{ data: user, code: 'SUCCESS' }
