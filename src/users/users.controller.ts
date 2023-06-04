66c2142e310d38b5b3bd0fe1310abc63399de38a
mck0324 <dch3030@naver.com>
2023-06-04 15:18:16 +0900

    controller -> service 데이터 넘겨보기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,12 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private userService: UsersService) {
+    }
+
     @Get()
     getUsers(@Req() req) {
         return req.user;
@@ -9,7 +14,7 @@ export class UsersController {
 
     @Post()
     postUsers(@Body() data: JoinRequestDto) {
- 
+        this.userService.postUsers(data.email, data.nickname, data.password);
     }
 
     @Post('login')
